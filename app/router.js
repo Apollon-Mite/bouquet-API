8fa448894f10755c0389ba4383ccc0e633bdc7ce
Apollon-Mite <abdoula.voraev@gmail.com>
2021-03-20 20:32:31 +0100

    added regex, now id must be an integer

--- app/router.js
+++ app/router.js
@@ -20,21 +20,21 @@ router.get('/', (req, res) => {
 
 /** Products */
 router.get('/products', productController.getAllProducts);
-router.get('/product/:id', productController.getOneProduct);
-router.get('/seller/:id/products', productController.getProductsFromSeller);
-router.post('/seller/:id/products', authorization, productController.addNewProduct)
-router.patch('/product/:id', productController.editOneProduct) // crée juste pour nous
+router.get('/product/:id(\\d+)', productController.getOneProduct);
+router.get('/seller/:id(\\d+)/products', productController.getProductsFromSeller);
+router.post('/seller/:id(\\d+)/products', authorization, productController.addNewProduct)
+router.patch('/product/:id(\\d+)', productController.editOneProduct) // crée juste pour nous
 
 // router.patch('/seller/:Sid/product/:Pid', productController.editOneProduct) // faudra ajouter la vérification par token ici
 
-router.patch('/image/:id', imageController.editOneImage)
+router.patch('/image/:id(\\d+)', imageController.editOneImage)
 
 
 /* Orders */
-router.get('/order/:id', orderController.getOneOrder);
-router.get('/seller/:id/orders', orderController.getSellerOrders); // TODO ajouter autorization
-router.get('/customer/:id/orders', orderController.getCustomerOrders); // TODO ajouter autorization
-router.post('/customer/:id/order', orderController.addNewOrder); // NEW ORDER
+router.get('/order/:id(\\d+)', orderController.getOneOrder);
+router.get('/seller/:id(\\d+)/orders', orderController.getSellerOrders); // TODO ajouter autorization
+router.get('/customer/:id(\\d+)/orders', orderController.getCustomerOrders); // TODO ajouter autorization
+router.post('/customer/:id(\\d+)/order', orderController.addNewOrder); // NEW ORDER
 
 
 
@@ -46,16 +46,16 @@ router.get('/categories', categoryController.getAllCategories);
 
 /* Customers */
 router.get('/customers', customerController.getAllCustomers);
-router.get('/customer/:id', customerController.getOneCustomer);
-router.patch('/customer/:id', authorization, customerController.editCustomerProfile);
+router.get('/customer/:id(\\d+)', customerController.getOneCustomer);
+router.patch('/customer/:id(\\d+)', authorization, customerController.editCustomerProfile);
 router.post('/customer/login', customerController.customerHandleLoginForm); // LOGIN
 router.post('/customer/signup', customerController.customerHandleSignupForm); // SIGNUP
 
 
 /* Sellers */
 router.get('/sellers', sellerController.getAllSellers);
-router.get('/seller/:id', sellerController.getOneSeller);
-router.patch('/seller/:id', /*authorization,*/ sellerController.editSellerProfile); // TODO remettre autorisation quand images changées
+router.get('/seller/:id(\\d+)', sellerController.getOneSeller);
+router.patch('/seller/:id(\\d+)', /*authorization,*/ sellerController.editSellerProfile); // TODO remettre autorisation quand images changées
 router.post('/seller/login', sellerController.sellerHandleLoginForm); // LOGIN
 router.post('/seller/signup', sellerController.sellerHandleSignupForm); // SIGNUP
 
